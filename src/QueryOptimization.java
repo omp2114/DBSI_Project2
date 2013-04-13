69043f208ae1b3556e4b8e6ef270215e593c6922
Orlando <omp2114@gmail.com>
2013-04-12 22:24:10 -0400

    Created algorithm class

---
+++ src/QueryOptimization.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class QueryOptimization {
+	private ArrayList<Double> selectivities;
+
+	public QueryOptimization(ArrayList<Double> s) {
+		selectivities = s;
+	}
+
+}
